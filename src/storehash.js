009cd7c3bd168104cc38d4f394d7e64f89f8cf93
zscole <zak@beattiecole.com>
2018-08-28 17:27:00 -0700

    add gitignore

--- src/storehash.js
+++ src/storehash.js
@@ -1,8 +1,5 @@
 import web3 from './web3';
-//access our local copy to contract deployed on rinkeby testnet
-//use your own contract address
 const address = '0xb84b12e953f5bcf01b05f926728e855f2d4a67a9';
-//use the ABI from your contract
 const abi = [
   {
     "constant": true,
